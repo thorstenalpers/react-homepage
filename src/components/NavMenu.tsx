8425b4c7ebfb3da91eb9600007b52cd5d21b6b72
netninja <netninja@gmx.de>
2020-05-20 22:17:10 +0200

    Fix highlight active link

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap';
 import Octicon, { MarkGithub } from '@primer/octicons-react'
 
 export default (props) => {
     const { location } = props;
+
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
@@ -11,10 +12,11 @@ export default (props) => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto" activeKey={location.pathname} >
-                        <Nav.Link eventKey="1" href="#Home">Über mich</Nav.Link>
-                        <Nav.Link eventKey="2" href="#Kenntnisse">Kenntnisse</Nav.Link>
-                        <Nav.Link eventKey="3" href="#Lebenslauf">Lebenslauf</Nav.Link>
+                        <Nav.Link eventKey="/Home" href="#Home">Über mich</Nav.Link>
+                        <Nav.Link eventKey="/Kenntnisse" href="#Kenntnisse">Kenntnisse</Nav.Link>
+                        <Nav.Link eventKey="/Lebenslauf" href="#Lebenslauf">Lebenslauf</Nav.Link>
                         <Nav.Link
+                            eventKey="github"
                             href="https://github.com/thorstenalpers"
                             target='_blank'
                             title="thorstenalpers - GitHub"
