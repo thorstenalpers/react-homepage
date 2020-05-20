9ef9de85bdd302788aeef836d6227d0b34c838a6
netninja <netninja@gmx.de>
2020-05-20 13:56:11 +0200

    Use devextreme styles in component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
