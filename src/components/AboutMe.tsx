ae4f0e68279f5a5614fa1c98bceb3644b51a2bae
netninja <netninja@gmx.de>
2020-08-02 12:43:31 +0200

    remove unused animation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
         <div className="centered-content__inner text display-4 ">
           <Typed
             strings={[
-              "Hi! Ich bin Thorsten Alpers.",
+              "Moin! Ich bin Thorsten Alpers.",
               "Ich bin ein Fullstack Softwareentwickler.",
               "Ich bin ein Teamplayer.",
               "Ich bin ein Softwarearchitekt.",
